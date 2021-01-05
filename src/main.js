634ed76173746b3a5cf7492b8a239970b16e5abf
hexbreak <theownerproject@gmail.com>
2021-01-05 04:15:20 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -6,5 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.addEventListener("keydown", function(e) {
-  console.log(e);
+  const audio = document.querySelector(`audio[data-keys="${e.keycode}"]`);
+  if (!audio) return; // stop the function from running all together
+
+  audio.play();
 });
