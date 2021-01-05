85bcbda53a57b02ba3af5540f9b18be53bf1ecc0
hexbreak <theownerproject@gmail.com>
2021-01-05 03:54:01 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+window.addEventListener("keydown", function(e) {
+  console.log(e);
+});
